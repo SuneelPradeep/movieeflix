c8f546e6d7ae6b9c7f1d10b0f937b6db8856bf6f
SuneelPradeep <ernestsolomoninzion@gmail.com>
2024-04-05 14:23:06 +0530

    changess

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    staticPageGenerationTimeout: 200000,
     experimental :{
         serverComponentsExternalPackages : ["mongoose"],
     },
